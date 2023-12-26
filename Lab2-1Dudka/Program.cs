6dcac71384ee8fb005a2829709517887e8d80ef5
B3dRandoms <bdrandoms0@gmail.com>
2023-12-26 14:54:38 +0200

    Initial commit

---
+++ Lab2-1Dudka/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+if (x >= y && x >= -y && x <= 1)
+{
+    Console.WriteLine("Принадлежит");
+}
+else
+{
+    Console.WriteLine("Не принадлежит");
+}
